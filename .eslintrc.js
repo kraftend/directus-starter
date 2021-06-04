e66d279ca50b98436fa1558a80cb2bdb25a2899d
cemreinanc <cemreinanc@gmail.com>
2021-06-04 17:28:54 +0300

    vue eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,22 @@
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  env: {
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    sourceType: 'module', // Allows for the use of imports
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
   },
   extends: [
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:vue/recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
